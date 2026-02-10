c182c16f3a878e472ef818cae191705a6f1a54d7
Alejandro Alonso <alejandro_alonso@apple.com>
2026-02-10 14:46:37 -0800

    Update abi test for Unique

--- test/abi/macOS/arm64/stdlib.swift
+++ test/abi/macOS/arm64/stdlib.swift
@@ -1282,3 +1282,11 @@ Added: _$ss15EmptyCollectionVyxGSHsWP
 
 // Wrapper for SWIFT_CONCURRENCY_TRACING_SUBSYSTEM environment variable
 Added: _concurrencyTracingSubsystem
+
+// Unique type metadata accessor
+Added: _$ss6UniqueVMa
+// Unique type descriptor
+Added: _$ss6UniqueVMn
+// Unique.pointer getter
+Added: _$ss6UniqueVsRi_zrlE7pointerSpyxGvg
+
