269e5a75fba1f31e8e30a68dc849351b146a87b1
Doug Gregor <dgregor@apple.com>
2026-04-30 16:07:24 -0700

    Make this an IR-checking test

--- test/embedded/floatingpointparsing.swift
+++ test/embedded/floatingpointparsing.swift
@@ -1,20 +1,15 @@
-// RUN: %target-run-simple-swift(-enable-experimental-feature Embedded -parse-as-library -wmo %target-embedded-posix-shim) | %FileCheck %s
+// RUN: %target-swift-frontend -emit-ir -o - %s -enable-experimental-feature Embedded -parse-as-library | %FileCheck %s
 
 // REQUIRES: swift_in_compiler
-// REQUIRES: executable_test
 // REQUIRES: optimized_stdlib
 // REQUIRES: swift_feature_Embedded
 
 // Check that initializing a Double with a string literal doesn't result in unresolved symbols
-@inline(never)
-func testLiteral() -> Double? {
-  Double("1.5")
-}
+// CHECK-NOT: stdlib_isOSVersionAtLeast
 
-@main
-struct Main {
-  static func main() {
-    print(testLiteral() == 1.5)
-    // CHECK: true
-  }
+// CHECK: define {{.*}} @"$e20floatingpointparsing11testLiteralSdSgyF"()
+public func testLiteral() -> Double? {
+  // CHECK: call swiftcc { i64, i8 } @"$eSdySdSgxcSyRzlufCSS_Tt0g5"
+  // CHECK-NOT: stdlib_isOSVersionAtLeast
+  Double("1.5")
 }
